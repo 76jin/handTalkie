1950473aa193dc12668761fb2960424a178e5548
KangJin <ranian129@gmail.com>
2014-06-09 11:57:49 +0900

    1. 이전에 수정했는데 빠진 파일들 추가로 커밋함.

--- talkie/src/main/webapp/js/common.js
+++ talkie/src/main/webapp/js/common.js
@@ -6,5 +6,18 @@ bit.getContextRootPath = function() {
 	return currPath.substring(0, end);
 }
 
+bit.getCookie = function(name) {
+  var cookies = document.cookie.split(';');
+  for (var i in cookies) {
+    if (cookies[i].search(name) != -1) {
+      return cookies[i]
+              .replace(name + '=', '')
+              .replace(/^\s\s*/,'')
+              .replace(/\s\s*$/,'');
+    }
+  }
+}
 bit.contextRoot = bit.getContextRootPath();
 bit.chatServerUrl = "192.168.200.10:9997";
+bit.userNo = bit.getCookie('userNo');
+bit.userEmail = bit.getCookie('loginEmail');
